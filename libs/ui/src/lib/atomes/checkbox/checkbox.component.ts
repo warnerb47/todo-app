94fdee62e80518756819f5dab76cf3699bef9352
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-07 12:29:35 +0000

    chore: add assets

--- libs/ui/src/lib/atomes/checkbox/checkbox.component.ts
+++ libs/ui/src/lib/atomes/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './checkbox.component.html',
-  styleUrl: './checkbox.component.css',
+  styleUrl: './checkbox.component.scss',
 })
-export class CheckboxComponent {}
+export class CheckboxComponent {
+  @Input() checked = true;
+}
