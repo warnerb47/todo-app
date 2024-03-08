b159bf779d62222aa2c19ed727e550107618d792
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-08 11:43:54 +0000

    refactor: refactor app component

--- apps/todo-app/src/app/app.component.ts
+++ apps/todo-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './views/header/header.component';
+import { TodoComponent } from './views/todo/todo.component';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TodoComponent],
   selector: 'todo-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
