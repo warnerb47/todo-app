3004df6a1fc875fc91dcc55fd11ba2236e7b3bf4
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-08 11:17:27 +0000

    feat: init header

--- apps/todo-app/src/app/app.component.ts
+++ apps/todo-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './views/header/header.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [HeaderComponent],
   selector: 'todo-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
