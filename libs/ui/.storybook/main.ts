42d472aa3ad80730fa14722019473a9ed262551c
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-08 15:47:49 +0000

    fix: fix storybook and app image

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     name: '@storybook/angular',
     options: {},
   },
-  staticDirs: [ '../src/assets' ],  
+  staticDirs: ['../../../libs/assets/src'],
 };
 
 export default config;
