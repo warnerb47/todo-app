ba1e5ccf247b647527a9bd867fe92cf1ee1b6db9
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-08 12:36:14 +0000

    feat: fix todo height

--- apps/todo-app/src/app/views/todo/todo.component.ts
+++ apps/todo-app/src/app/views/todo/todo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewTaskComponent, TaskListComponent } from '@todo-app/ui';
 
 @Component({
   selector: 'todo-app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NewTaskComponent, TaskListComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
