23a9716b0c692aa67e64c63f82e00e604954f51e
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-11 09:39:27 +0000

    fix: fix task list design

--- apps/todo-app/src/app/views/todo/todo.component.ts
+++ apps/todo-app/src/app/views/todo/todo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewTaskComponent, TaskListComponent } from '@todo-app/ui';
 import { Task } from 'libs/ui/src/core/models/task';
+import { StubTaskBuilder } from 'libs/ui/src/core/models/builders/task.builder';
 
 @Component({
   selector: 'todo-app-todo',
@@ -11,7 +12,12 @@ import { Task } from 'libs/ui/src/core/models/task';
   styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
-  
+  tasks: Task[] = [
+    new StubTaskBuilder().withIndex(0).withLabel('Init project').withChecked(true).build(),
+    new StubTaskBuilder().withIndex(1).withLabel('Setup CI/CD').build(),
+    new StubTaskBuilder().withIndex(2).withLabel('Init test').build(),
+  ];
+
   createTask(task: Task): void {
     console.log(task);
   }
