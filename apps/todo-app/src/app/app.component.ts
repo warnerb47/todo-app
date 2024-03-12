ca41438b483e37b3cdeadb2480f46a114264e745
warnerb47 <taphagaye2001.mg@gmail.com>
2024-03-12 15:14:05 +0000

    chore: add lib local assets

--- apps/todo-app/src/app/app.component.ts
+++ apps/todo-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './views/header/header.component';
 import { TodoComponent } from './views/todo/todo.component';
+import { MyPublicLibComponent } from 'my-public-lib';
+// import { MyPublicLibComponent } from '@todo-app/my-public-lib';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, TodoComponent],
+  imports: [HeaderComponent, TodoComponent, MyPublicLibComponent],
   selector: 'todo-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
